b7b0faa0bebb3049814998542ab20fef74e1180a
Luis Alfredo Porras Páez <lporras16@gmail.com>
2022-08-14 15:38:20 -0500

    maps with leaflet

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,30 +1,54 @@
-// import { Controller } from "@hotwired/stimulus"
-// import L from "leaflet"
-// import * as GeoSearch from "leaflet-geosearch"
-
-// export default class extends Controller {
-//   static targets = ["container"]
-
-//   containerTargetConnected() {
-//     this.map = L.map(this.containerTarget).setView([51.505, -0.09], 13);
-
-//     L.tileLayer('//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
-
-//     const search = new GeoSearch.GeoSearchControl({
-//       style: 'bar',
-//       provider: new GeoSearch.OpenStreetMapProvider(),
-//     });
-
-//     this.map.addControl(search);
-//   }
-
-//   disconnect(){
-//     this.map.remove()
-//   }
-// }
+import { Controller } from "@hotwired/stimulus"
+import L from "leaflet"
+import * as GeoSearch from "leaflet-geosearch"
 
 export default class extends Controller {
-  connect() {
-    console.log("map controller")
+  static targets = ["container", "address", "latitude", "longitude"]
+
+  containerTargetConnected() {
+    let defaultLocation = [51.505, -0.09]
+
+    if (this.latitudeTarget.value.length > 0 && this.longitudeTarget.value.length > 0) {
+      defaultLocation = [this.latitudeTarget.value, this.longitudeTarget.value]
+    }
+
+    this.map = L.map(this.containerTarget).setView(defaultLocation, 18);
+    const provider = new GeoSearch.OpenStreetMapProvider();
+
+    L.tileLayer('//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
+
+    const search = new GeoSearch.GeoSearchControl({
+      style: 'bar',
+      provider: provider
+    });
+
+    this.map.addControl(search);
+
+    this.map.on('geosearch/showlocation', (event) => {
+      let latLng = event.marker.getLatLng()
+
+      this.addressTarget.value = event.location.label
+      this.latitudeTarget.value = latLng.lat
+      this.longitudeTarget.value = latLng.lng
+    });
+
+    this.map.on('geosearch/marker/dragend', (event) => {
+      this.latitudeTarget.value = event.location.lat
+      this.longitudeTarget.value = event.location.lng
+    })
+
+    if (this.addressTarget.value.length > 0) {
+      let query = { query: this.addressTarget.value }
+
+      provider.search(query).then((result) => {
+        search.showResult(result[0], query)
+      });
+
+      search.searchElement.input.value = this.addressTarget.value
+    }
+  }
+
+  disconnect(){
+    this.map.remove()
   }
 }
